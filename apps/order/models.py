02125a71e3f5ec5759e68b505e6d1a61b4dc5bd4
Fatemeh Aghaie <fatemehaghaie2020math@gmail.com>
2024-04-19 13:12:30 +0330

    edir model orederitem for edit error

--- apps/order/models.py
+++ apps/order/models.py
@@ -1,14 +1,41 @@
-from django.conf import settings
 from django.db import models
 from apps.shop.models import Product
 from apps.user.models import User
 
 
-class OrderItem(models.Model):
+class Coupon(models.Model):
+    code = models.CharField(max_length=15)
+    amount = models.FloatField()
+
+    def __str__(self):
+        return self.code
+
+
+class Order(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+    start_date = models.DateTimeField(auto_now_add=True)
+    ordered_date = models.DateTimeField(auto_now=True)
+    status = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.user.username
+
+    def get_total(self):
+        total = 0
+        for order_item in self.items.all():
+            total += order_item.get_final_price()
+        if self.coupon:
+            total -= self.coupon.amount
+        return total
+
+
+class OrderItem(models.Model):
     ordered = models.BooleanField(default=False)
     item = models.ForeignKey(Product, on_delete=models.CASCADE)
     quantity = models.IntegerField(default=1)
+    order = models.ForeignKey(
+        Order, on_delete=models.CASCADE, default=None
+    )  # Add default=None here
 
     def __str__(self):
         return f"{self.quantity} of {self.item.name}"
@@ -26,30 +53,3 @@ class OrderItem(models.Model):
         if self.item.discount_price:
             return self.get_total_discount_item_price()
         return self.get_total_item_price()
-
-
-class Coupon(models.Model):
-    code = models.CharField(max_length=15)
-    amount = models.FloatField()
-
-    def __str__(self):
-        return self.code
-
-
-class Order(models.Model):
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    items = models.ManyToManyField(OrderItem)
-    start_date = models.DateTimeField(auto_now_add=True)
-    ordered_date = models.DateTimeField(auto_now=True)
-    coupon = models.ForeignKey(Coupon, on_delete=models.SET_NULL, blank=True, null=True)
-
-    def __str__(self):
-        return self.user.username
-
-    def get_total(self):
-        total = 0
-        for order_item in self.items.all():
-            total += order_item.get_final_price()
-        if self.coupon:
-            total -= self.coupon.amount
-        return total
