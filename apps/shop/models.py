871ffb4f2103a9989a0c4d67a4cae71d61a1fcf9
Fatemeh Aghaie <fatemehaghaie2020math@gmail.com>
2024-04-19 08:06:31 +0330

    add model category and product

--- apps/shop/models.py
+++ apps/shop/models.py
@@ -1,1 +1,53 @@
-# Create your models here.
+from django.db import models
+from django.template.defaultfilters import slugify
+
+
+class Category(models.Model):
+    title = models.CharField(max_length=200)
+    image = models.ImageField(upload_to="media", blank=False)
+    sub_category = models.ForeignKey(
+        "self",
+        on_delete=models.CASCADE,
+        related_name="sub_categories",
+        null=True,
+        blank=True,
+    )
+    is_actived = models.BooleanField(default=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+    def save(self, *args, **kwargs):  # new
+        self.slug = slugify(self.title)
+        return super().save(*args, **kwargs)
+
+
+class Product(models.Model):
+    category = models.ForeignKey(
+        Category, on_delete=models.CASCADE, related_name="category"
+    )
+    image = models.ImageField(upload_to="media")
+    name = models.CharField(max_length=250)
+    description = models.TextField()
+    price = models.IntegerField(default=0)
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_updated = models.DateTimeField(auto_now=True)
+    slug = models.SlugField(unique=True)
+    is_actived = models.BooleanField(default=True)
+
+    class Meta:
+        ordering = ("-date_created",)
+
+    def __str__(self):
+        return self.slug
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        return super().save(*args, **kwargs)
+
+    @property
+    def can_be_added_to_cart(self):
+        return self.is_actived
