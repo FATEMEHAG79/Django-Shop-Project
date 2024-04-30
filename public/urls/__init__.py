c38641b1e6b7ad2c5eaea68f66b27d3d1b9bb0e5
FATEMEHAG79 <fatemehaghai2020math@gmail.com>
2024-04-30 21:21:18 +0430

    ✨Develop 02 |  write url contact view.

--- public/urls/__init__.py
+++ public/urls/__init__.py
@@ -4,5 +4,10 @@ from django.urls import path
 urlpatterns = [
     path("", template.HomeView.as_view(), name="home"),
     path("Aboutus/", template.AboutView.as_view(), name="about"),
+    path(
+        "Contact_us/",
+        template.ContactUs.as_view(),
+        name="contact",
+    ),
 
 ]
