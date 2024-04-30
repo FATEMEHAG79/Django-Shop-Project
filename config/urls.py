357e5995beff1e1d3acee817717ef0a1cd00e837
FATEMEHAG79 <fatemehaghai2020math@gmail.com>
2024-04-30 20:59:09 +0430

    ✨Develop 02 |  Add urls package in user app and add in to urls config.

--- config/urls.py
+++ config/urls.py
@@ -4,11 +4,12 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.views.generic import TemplateView
 from django.conf.urls.i18n import i18n_patterns
+from django.urls import path, include
 
 urlpatterns = i18n_patterns(
     path("admin/", admin.site.urls),
     path("", TemplateView.as_view(template_name="home.html"), name="home"),
-    path("login/", TemplateView.as_view(template_name="sign_up_in.html"), name="login"),
+    path("", include("apps.user.urls")),
     path("Aboutus/", TemplateView.as_view(template_name="about.html"), name="about"),
     prefix_default_language=False,
 )
