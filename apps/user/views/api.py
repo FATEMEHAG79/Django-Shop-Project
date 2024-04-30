7405d040b11c44c3badd8a3e1a6d746c61e43b85
FATEMEHAG79 <fatemehaghai2020math@gmail.com>
2024-04-30 21:37:55 +0430

    ✨Develop 02 |  Add log in ith user name/email with password view in api file in view package.

--- apps/user/views/api.py
+++ apps/user/views/api.py
@@ -63,3 +63,44 @@ class RegisterView(generic.CreateView):
                 )
 
                 return redirect("send_email")
+
+
+            class LoginView(generic.View):
+                template_name = "auth/sign_in.html"
+
+                def get(self, request):
+                    return render(request, self.template_name)
+
+                def post(self, request):
+                    username = self.request.POST.get("username", None)
+                    password = self.request.POST.get("password", None)
+
+                    if not all((username, password)):
+                        return response.HttpResponse(
+                            "'username' or 'password' should not be null !", status=400
+                        )
+
+                    if user := authenticate(password=password, username=username):
+                        if user.is_active:
+                            login(self.request, user)
+                            return redirect("home")
+
+                        # activate
+                        if not cache.get_or_create(
+                                f"activate_token_user_{user.username}", lambda: uuid4().hex, 300
+                        ):
+                            # send mail !
+                            mail.send_mail(
+                                f"Verification {user.username}",
+                                user.email,
+                                "mail/verify.html",
+                                {
+                                    "user": user,
+                                    "token": cache.cache.get(
+                                        f"activate_token_user_{user.username}"
+                                    ),
+                                    "host": self.request.get_host(),
+                                },
+                            )
+                            return redirect("send_email")
+                    return response.HttpResponse("User not found !", status=404)
