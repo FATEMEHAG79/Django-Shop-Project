6aa8e3d7ec6359ec9ebc29b8e7298caae63ad060
FATEMEHAG79 <fatemehaghai2020math@gmail.com>
2024-04-30 21:25:08 +0430

    ✨Develop 02 |  edit urls config and remove some urls.

--- config/urls.py
+++ config/urls.py
@@ -1,19 +1,18 @@
 from django.contrib import admin
-from django.urls import path
 from django.conf import settings
-from django.conf.urls.static import static
-from django.views.generic import TemplateView
 from django.conf.urls.i18n import i18n_patterns
 from django.urls import path, include
 
 urlpatterns = i18n_patterns(
-    path("admin/", admin.site.urls),
     path("", include("public.urls")),
-    path("", TemplateView.as_view(template_name="home.html"), name="home"),
     path("", include("apps.user.urls")),
-    path("Aboutus/", TemplateView.as_view(template_name="about.html"), name="about"),
     prefix_default_language=False,
 )
-urlpatterns = urlpatterns + static(
-    settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
-)
+if settings.DEBUG:
+    from django.conf.urls.static import static
+
+    urlpatterns += (
+        [path("admin/", admin.site.urls)]
+        + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+        + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    )
