ca048bef5371811e6106113f7a81e62705fb6ce7
FATEMEHAG79 <fatemehaghaie2020math@gmail.com>
2024-05-12 23:29:21 +0330

    ✨Develop 02 | add url for category & product detail.

--- apps/shop/urls/__init__.py
+++ apps/shop/urls/__init__.py
@@ -1,5 +1,5 @@
 from django.urls import path, include
 
 urlpatterns = [
-    path("", include("apps.user.urls.auth")),
+    path("", include("apps.shop.urls.product")),
 ]
---
+++ apps/shop/urls/product.py
@@ -0,0 +1,18 @@
+from apps.shop.views import template
+from django.urls import path
+
+urlpatterns = [
+    path(
+        "shop/<str:slug>/", template.CategoryDetailView.as_view(), name="categorydetail"
+    ),
+    path(
+        "shop/<str:slug>/<str:slug1>",
+        template.SubCategoryDetailView.as_view(),
+        name="subcategorydetail",
+    ),
+    path(
+        "shop/product/<str:slug>/<str:slug1>",
+        template.ProductDetailView.as_view(),
+        name="productdetail",
+    ),
+]
