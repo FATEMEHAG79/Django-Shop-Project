3e21b263a43d30b7e34637b55f35be13a1a1356c
FATEMEHAG79 <fatemehaghaie2020math@gmail.com>
2024-05-17 02:10:34 +0330

    ✨Develop 03 | add query media file for image.

--- apps/shop/views/template.py
+++ apps/shop/views/template.py
@@ -1,51 +1,82 @@
 from django.views.generic import DetailView
-from apps.shop.models import Product,Category,Brand
+from apps.shop.models import Product, Category, Brand,Media
 from django.shortcuts import render
 
 
 class CategoryDetailView(DetailView):
-    model = Product,Category
+    model = Product, Category
 
-    def get(self, request,slug):
+    def get(self, request, slug):
         itemcategory = Category.objects.filter(parent__slug=slug)
-        productlist = Product.objects.select_related("category__parent").filter(category__parent__slug=slug)
+        productlist = Product.objects.select_related("category__parent").filter(
+            category__parent__slug=slug
+        )
+        media_file= Media.objects.filter(product__category__parent__slug=slug)
+        if "cart_data_obj" in request.session:
+            totalcartitems = sum(
+                item["qty"] for item in request.session["cart_data_obj"].values()
+            )
+        else:
+            totalcartitems = 0
         category = Category.objects.filter(parent=None)
-        brands= Brand.objects.all()
-        context={
-            "itemcategory":itemcategory,
+        brands = Brand.objects.all()
+        context = {
+            "itemcategory": itemcategory,
             "productlist": productlist,
             "category": category,
-            "brands":brands
+            "brands": brands,
+            "media" :media_file,
+            "totalcartitems": totalcartitems,
         }
 
-        return render(request,"product/category-detail.html",context)
-
+        return render(request, "product/category-detail.html", context)
 
 
 class SubCategoryDetailView(DetailView):
-    model = Product,Category
-    def get(self, request,slug,slug1):
-        productlist = Product.objects.select_related("category").filter(category__slug=slug1)
+    model = Product, Category
+
+    def get(self, request, slug, slug1):
+        productlist = Product.objects.select_related("category").filter(
+            category__slug=slug1
+        )
+        media_file = Media.objects.filter(product__category__parent__slug=slug)
+        if "cart_data_obj" in request.session:
+            totalcartitems = sum(
+                item["qty"] for item in request.session["cart_data_obj"].values()
+            )
+        else:
+            totalcartitems = 0
         category = Category.objects.filter(parent=None)
-        brands= Brand.objects.all()
-        content={
+        brands = Brand.objects.all()
+        content = {
             "productlist": productlist,
             "category": category,
-            "brands":brands
+            "brands": brands,
+            "media": media_file,
+            "totalcartitems": totalcartitems,
         }
 
-        return render(request,"product/subcategory_detail.html",content)
-
+        return render(request, "product/subcategory_detail.html", content)
 
 
 class ProductDetailView(DetailView):
     model = Product
-    def get(self, request,slug,slug1):
+
+    def get(self, request, slug, slug1):
         product = Product.objects.get(slug=slug1)
         category = Category.objects.filter(parent=None)
-        content={
-            "product":product,
+        media_file = Media.objects.filter(product__slug=slug1)
+        if "cart_data_obj" in request.session:
+            totalcartitems = sum(
+                item["qty"] for item in request.session["cart_data_obj"].values()
+            )
+        else:
+            totalcartitems = 0
+        content = {
+            "product": product,
             "category": category,
+            "media": media_file,
+            "totalcartitems": totalcartitems,
         }
 
-        return render(request,"product/product_detail.html",content)
+        return render(request, "product/product_detail.html", content)
