bd7e04c7942056715591a14bc7ecd8d86fad254b
FATEMEHAG79 <fatemehaghaie2020math@gmail.com>
2024-05-28 22:11:07 +0330

    ✨Develop 03 | add url list item in cart and delete item from cart.

--- apps/order/urls/cart.py
+++ apps/order/urls/cart.py
@@ -1,5 +1,9 @@
 from django.urls import path
-from apps.order.views.api import AddToCartAPI
+from apps.order.views.api import AddToCartAPI, CartViewAPI,DeleteItemCart
 
 
-urlpatterns = [path("add-to-cart/", AddToCartAPI.as_view(), name="add-to-cart")]
+urlpatterns = [
+    path("add-to-cart/", AddToCartAPI.as_view(), name="add-to-cart"),
+    path("cart/", CartViewAPI.as_view(), name="cart"),
+    path("delete-product/",DeleteItemCart.as_view(),name="delete-product")
+]
