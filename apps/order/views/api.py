594260ffdfddd137bd7521f1ca8790dd24d1ce80
FATEMEHAG79 <fatemehaghaie2020math@gmail.com>
2024-05-29 15:09:24 +0330

    ✨Develop 03 | set up update item view in cart list.

--- apps/order/views/api.py
+++ apps/order/views/api.py
@@ -106,3 +106,44 @@ class DeleteItemCart(APIView):
         else:
             return render(request, template_name="cart/cart-empty.html")
 
+
+
+class UpdateItemCart(APIView):
+    def get(self, request):
+        item_id=str(request.GET["id"])
+        item_qty=int(request.GET["qty"])
+        if "cart_data_obj" in request.session:
+            if item_id in request.session["cart_data_obj"]:
+                cart_data=request.session["cart_data_obj"]
+                cart_data[str(request.GET["id"])]["qty"]=item_qty
+                request.session["cart_data_obj"]=cart_data
+        cart_total_amount = 0
+        if "cart_data_obj" in request.session:
+            cart_data = request.session["cart_data_obj"]
+            for p_id, item in cart_data.items():
+                item["totla_price"] = int(item["qty"]) * float(item["price"])
+                product = Product.objects.get(id=p_id)
+                item["slug_category"] = product.category.slug
+                item["slug"] = product.slug
+                media_files = Media.objects.filter(product=product)
+                item["media"] = [{"url":media.file.url}for media in media_files]
+                cart_total_amount += int(item["qty"]) * float(item["price"])
+            cart_data = request.session["cart_data_obj"]
+            total_cart_items = sum(
+                item["qty"] for item in request.session["cart_data_obj"].values()
+            )
+            context=render_to_string("cart/cart_list.html", {
+                    "cart_data": cart_data,
+                    "total_cart_items": total_cart_items,
+                    "cart_total_amount": cart_total_amount,
+                })
+            return Response(
+                {
+                    "data" : context,
+                    "totalcartitems": sum(
+                    item["qty"] for item in request.session["cart_data_obj"].values()),
+                    "cart_total_amount": cart_total_amount,
+                }
+            )
+        else:
+            return render(request, template_name="cart/cart-empty.html")
