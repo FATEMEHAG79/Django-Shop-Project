f37705cb61eabb49c16c10ba71f63a99d75944f2
FATEMEHAG79 <fatemehaghaie2020math@gmail.com>
2024-05-29 13:10:01 +0330

    ✨Develop 03 | edit media query for image product.

--- apps/core/context_processors.py
+++ apps/core/context_processors.py
@@ -1,4 +1,4 @@
-from apps.shop.models import Category, Brand
+from apps.shop.models import Category, Brand, Product
 
 
 def myquery(request):
@@ -8,9 +8,19 @@ def myquery(request):
         )
     else:
         totalcartitems = 0
+    cart_total_amount = 0
+    cart_data = request.session["cart_data_obj"]
+    for p_id, item in cart_data.items():
+        item["totla_price"] = int(item["qty"]) * float(item["price"])
+        product = Product.objects.get(id=p_id)
+        item["slug_category"] = product.category.slug
+        item["slug"] = product.slug
+        cart_total_amount += int(item["qty"]) * float(item["price"])
+    cart_data = request.session["cart_data_obj"]
     context = {
         "category": Category.objects.filter(parent=None),
         "totalcartitems": totalcartitems,
+        "cart_data": cart_data,
         "brands" : Brand.objects.all()
     }
     return context
