906f2d15fe539034b19d7d81233e23f01c0417ae
FATEMEHAG79 <fatemehaghaie2020math@gmail.com>
2024-05-29 15:09:47 +0330

    ✨Develop 03 | add url update .

--- apps/order/urls/cart.py
+++ apps/order/urls/cart.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from apps.order.views.api import AddToCartAPI, CartViewAPI,DeleteItemCart
+from apps.order.views.api import AddToCartAPI, CartViewAPI,DeleteItemCart,UpdateItemCart
 
 
 urlpatterns = [
     path("add-to-cart/", AddToCartAPI.as_view(), name="add-to-cart"),
     path("cart/", CartViewAPI.as_view(), name="cart"),
-    path("delete-product/",DeleteItemCart.as_view(),name="delete-product")
+    path("delete-product/",DeleteItemCart.as_view(),name="delete-product"),
+    path("update-product/",UpdateItemCart.as_view(),name="update-product")
 ]
