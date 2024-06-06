e0ae5f2d488ec511a93b8205a1c0f162cdd97850
FATEMEHAG79 <fatemehaghaie2020math@gmail.com>
2024-06-07 01:18:55 +0330

    ✨Develop 03 | add url checkout

--- apps/order/urls/cart.py
+++ apps/order/urls/cart.py
@@ -1,10 +1,15 @@
 from django.urls import path
-from apps.order.views.api import AddToCartAPI, CartViewAPI,DeleteItemCart,UpdateItemCart
+from apps.order.views.api import AddToCartAPI, CartViewAPI,DeleteItemCart,UpdateItemCart,Checkout,EditAddress
+
+
 
 
 urlpatterns = [
     path("add-to-cart/", AddToCartAPI.as_view(), name="add-to-cart"),
     path("cart/", CartViewAPI.as_view(), name="cart"),
     path("delete-product/",DeleteItemCart.as_view(),name="delete-product"),
-    path("update-product/",UpdateItemCart.as_view(),name="update-product")
+    path("update-product/",UpdateItemCart.as_view(),name="update-product"),
+    path("checkout/<str:slug>/",Checkout.as_view(),name="checkout"),
+    path("checkout/", EditAddress.as_view(), name="editaddress"),
+    # path("checkout/", CreateAddress.as_view(), name="createaddress"),
 ]
