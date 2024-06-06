e77bb0ba49e01827c1838e1d65fae942af531f28
FATEMEHAG79 <fatemehaghaie2020math@gmail.com>
2024-06-07 01:19:47 +0330

    ✨Develop 03 | add context peocessor for remove simmlar code.

--- apps/core/context_processors.py
+++ apps/core/context_processors.py
@@ -1,29 +1,33 @@
-from apps.shop.models import Category, Brand, Product,Media
+from apps.shop.models import Category, Brand, Product, Media
 
 
 def myquery(request):
+    # بررسی وجود session و cart_data_obj در session
     if "cart_data_obj" in request.session:
+        cart_data = request.session["cart_data_obj"]
         totalcartitems = sum(
-            item["qty"] for item in request.session["cart_data_obj"].values()
+            item["qty"] for item in cart_data.values()
         )
     else:
+        cart_data = {}
         totalcartitems = 0
+
     cart_total_amount = 0
-    cart_data = request.session["cart_data_obj"]
+
     for p_id, item in cart_data.items():
-        item["totla_price"] = int(item["qty"]) * float(item["price"])
+        item["total_price"] = int(item["qty"]) * float(item["price"])
         product = Product.objects.get(id=p_id)
         item["slug_category"] = product.category.slug
         item["slug"] = product.slug
-        media_files=Media.objects.filter(product=product)
-        item["media"]=media_files
+        media_files = Media.objects.filter(product=product)
+        item["media"] = media_files
         cart_total_amount += int(item["qty"]) * float(item["price"])
-    cart_data = request.session["cart_data_obj"]
+
     context = {
         "category": Category.objects.filter(parent=None),
         "totalcartitems": totalcartitems,
         "cart_data": cart_data,
         "cart_total_amount": cart_total_amount,
-        "brands" : Brand.objects.all()
+        "brands": Brand.objects.all()
     }
     return context
