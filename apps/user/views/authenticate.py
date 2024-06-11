b2f558459cc33a2d86d4fa1b8e40c1d66f27967c
FATEMEHAG79 <fatemehaghaie2020math@gmail.com>
2024-06-11 20:04:05 +0330

    ✨Develop 04 | Rename view and & delete additional code.

--- apps/user/views/authenticate.py
+++ apps/user/views/authenticate.py
@@ -1,6 +1,6 @@
 from apps.user.models import User
 from uuid import uuid4
-from django.http import response
+from django.http import response, HttpResponse
 from utils import cache, mail
 from django.views import generic
 from django.contrib import messages
@@ -16,13 +16,10 @@ class SendOtp(generic.CreateView):
 
     def post(self, request):
         email = request.POST.get("email", None)
-        if not email:
-            messages.info(request, "email should not be Null.")
-            return redirect("signup/in")
         # otp
         token = uuid4().hex
-        if not cache.get_or_create(email, token, 120):
-            code = "".join([car for car in token if car.isnumeric()][0:4])
+        code = "".join([car for car in token if car.isnumeric()][0:4])
+        if not cache.get_or_create(email, code, 120):
             # send mail !
             mail.send_mail(
                 f"Verification {email}",
@@ -32,35 +29,39 @@ class SendOtp(generic.CreateView):
             )
             user = User.objects.filter(email=email).exists()
             if user:
-                return redirect("confirmotp", email, token)
+                return redirect(f"confirmotp/?email={email}")
             else:
-                return redirect("confirmotpregister", email, token)
-        messages.info(request, "otp send !please try after 3 minitue.")
-        return redirect("signup/in")
+                return redirect(f"confirmregistration/?email={email}")
+        success = "otp send !please try after 3 minitue."
+        return HttpResponse(success)
 
 
 class ConfirmOtp(generic.View):
     template_name = "auth/sign_in.html"
 
-    def get(self, request, email, token):
-        context = {"email": email, "token": token}
-        return render(request, self.template_name, context)
+    def get(self, request):
+        email = request.GET.get("email")
+        return render(request, self.template_name, {"email": email})
 
-    def post(self, request, token, email):
+    def post(self, request):
         otp = self.request.POST.get("otp", None)
+        email = self.request.POST.get("email")
         if not otp:
             return response.HttpResponse("otp should not be null !", status=400)
-        code = "".join([car for car in token if car.isnumeric()][0:4])
+        code = cache.cache.get(email)
         if otp == code:
             user = User.objects.get(email=email)
-            if user := authenticate(password=user.password, username=user.username):
+            if user.DoesNotExist:
                 if user.is_active:
+                    cache.cache.delete(email)
                     login(self.request, user)
                     return redirect("home")
                 # activate
                 if not cache.get_or_create(
                     f"activate_token_user_{user.username}", lambda: uuid4().hex, 120
                 ):
+                    cache.cache.delete(email)
+                    login(self.request, user)
                     # send mail !
                     mail.send_mail(
                         f"Verification {user.username}",
@@ -75,39 +76,36 @@ class ConfirmOtp(generic.View):
                         },
                     )
                     return redirect("send_email")
-                messages.info(
-                    request, "email verification send !please try after 2 minitue."
+                return HttpResponse(
+                    "email verification send !please try after 2 minitue."
                 )
-                return redirect("send_email")
         else:
-            messages.info(request, "otp is not correct.")
-            return redirect("confirmotp", email, token)
+            return HttpResponse("otp is not correct.")
 
 
 class LoginView(generic.View):
     template_name = "auth/sign_in.html"
 
-    def get(self, request, email, token):
-        context = {"email": email, "token": token}
-        return render(request, self.template_name, context)
+    def get(self, request):
+        email = request.GET.get("email")
+        return render(request, self.template_name, {"email": email})
 
-    def post(self, request, email, token):
+    def post(self, request):
+        email = self.request.POST.get("email")
         username = self.request.POST.get("username", None)
         password = self.request.POST.get("password", None)
 
-        if not all((username, password)):
-            messages.info(request, "'username' or 'password' should not be null !")
-            return redirect("home")
-
         if user := authenticate(password=password, username=username):
             if user.is_active:
+                cache.cache.delete(email)
                 login(self.request, user)
                 return redirect("home")
-
             # activate
             if not cache.get_or_create(
                 f"activate_token_user_{user.username}", lambda: uuid4().hex, 120
             ):
+                cache.cache.delete(email)
+                login(self.request, user)
                 # send mail !
                 mail.send_mail(
                     f"Verification {user.username}",
@@ -122,47 +120,43 @@ class LoginView(generic.View):
                     },
                 )
                 return redirect("send_email")
-            messages.info("otp send !please try after 3 minitue.")
-            return redirect("login")
+            return HttpResponse("otp send !please try after 3 minitue.")
+        return HttpResponse("username or password is not correct.please try again")
 
 
 class ConfirmOtpRegister(generic.View):
     template_name = "auth/confirmotp_signup.html"
 
-    def get(self, request, email, token):
-        context = {"email": email, "token": token}
-        return render(request, self.template_name, context)
+    def get(self, request):
+        email = request.GET.get("email")
+        return render(request, self.template_name, {"email": email})
 
-    def post(self, request, email, token):
+    def post(self, request):
+        email = self.request.POST.get("email")
         otp = self.request.POST.get("otp", None)
         if not otp:
             messages.info(request, "otp should not be Null.")
-            return redirect("confirmotpregister", email, token)
-        token_ = cache.cache.get(email)
-        code = "".join([car for car in token if car.isnumeric()][0:4])
+            return redirect(f"confirmregistration/?email={email}")
+
+        code = cache.cache.get(email)
         if otp == code:
             user = User.objects.create_user(self, email=email)
             user.save()
             return redirect("register", email)
-        messages.info(request, "otp is not correct.")
-        return redirect("register", email, token)
+        return HttpResponse("otp is not correct.")
 
 
 class Registeration(generic.View):
     template_name = "auth/register.html"
 
     def get(self, request, email):
-        context = {"email": email}
-        return render(request, self.template_name, context)
+        return render(request, self.tempclate_name, {"email": email})
 
     def post(self, request, email):
         username = self.request.POST.get("username", None)
         phone_number = self.request.POST.get("phone_number", None)
         password = self.request.POST.get("password", None)
         confirmpassword = self.request.POST.get("password1", None)
-        if not all((username, password, confirmpassword)):
-            messages.info(request, "this field should not be Null.")
-            return redirect("register", email)
         if password == confirmpassword:
             user = User.objects.get(email=email)
             user.username = username
@@ -173,6 +167,8 @@ class Registeration(generic.View):
             if not cache.get_or_create(
                 f"activate_token_user_{user.username}", lambda: uuid4().hex, 120
             ):
+                cache.cache.delete(email)
+                login(self.request, user)
                 # send mail !
                 mail.send_mail(
                     f"Verification {user.username}",
@@ -187,5 +183,4 @@ class Registeration(generic.View):
                     },
                 )
                 return redirect("send_email")
-        messages.info(request, "password and confirm password is not match.")
-        return redirect("register", email)
+        return HttpResponse("password and confirm password is not match.")
