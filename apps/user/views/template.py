ffca152f9dd870475f44e9e01b2cbaabcee3c653
FATEMEHAG79 <fatemehaghaie2020math@gmail.com>
2024-06-11 10:59:47 +0330

    ✨Develop 04 | Fix user authentication check in Activate profile view.

--- apps/user/views/template.py
+++ apps/user/views/template.py
@@ -1,3 +1,4 @@
+from uuid import uuid4
 from apps.order.models import Order
 from utils import cache
 from django.http import response
@@ -6,6 +7,7 @@ from django.shortcuts import get_object_or_404, render
 from django.contrib.auth import get_user_model, login, logout
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import DetailView
+from utils import mail
 from django.shortcuts import redirect
 
 User = get_user_model()
@@ -43,9 +45,9 @@ class ProfileView(DetailView, LoginRequiredMixin):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         user = context["user"]
-        order_completed=Order.objects.filter(user=user,status=True)
+        order_completed = Order.objects.filter(user=user, status=True)
         order_incompleted = Order.objects.filter(user=user, status=False)
-        context["order_completed"]=order_completed
+        context["order_completed"] = order_completed
         context["order_incompleted"] = order_incompleted
         return context
 
@@ -64,7 +66,7 @@ class EditProfile(generic.RedirectView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        user = context["user"]
+        context["user"]
         return context
 
     def post(self, request):
@@ -73,14 +75,39 @@ class EditProfile(generic.RedirectView):
         last_name = self.request.POST.get("last_name")
         email = self.request.POST.get("email")
         phone_number = self.request.POST.get("phone_number")
-        gender=self.request.POST.get("gender")
+        gender = self.request.POST.get("gender")
         data = {
             "first_name": first_name,
-            "gender":gender,
+            "gender": gender,
             "last_name": last_name,
             "email": email,
             "phone_number": phone_number,
             "username": username,
         }
-        user = User.objects.filter(email=email).update(**data)
+        User.objects.filter(email=email).update(**data)
         return redirect("profile", User.objects.get(email=email).slug)
+
+
+class ActivateProfile(generic.RedirectView):
+    def post(self, request):
+        user = request.user
+        if user.is_authenticated:
+            if not user.is_active:
+                if not cache.get_or_create(
+                    f"activate_token_user_{user.username}", lambda: uuid4().hex, 120
+                ):
+                    # send mail !
+                    mail.send_mail(
+                        f"Verification {user.username}",
+                        user.email,
+                        "auth/verify.html",
+                        {
+                            "user": user,
+                            "token": cache.cache.get(
+                                f"activate_token_user_{user.username}"
+                            ),
+                            "host": self.request.get_host(),
+                        },
+                    )
+                    return redirect("send_email")
+        return redirect("profile", User.objects.get(user=user).slug)
