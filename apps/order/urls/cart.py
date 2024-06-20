b33eee6fd25b4838dc048a77f759561b1e4437d1
FATEMEHAG79 <fatemehaghaie2020math@gmail.com>
2024-06-21 00:23:20 +0330

    ✨Develop 04 | Add url copoun.

--- apps/order/urls/cart.py
+++ apps/order/urls/cart.py
@@ -1,17 +1,23 @@
 from django.urls import path
-from apps.order.views.api import AddToCartAPI, CartViewAPI,DeleteItemCart,UpdateItemCart,Checkout,EditAddress,PlaceOrderView,ConfirmOrderView
-
-
+from apps.order.views.api import (
+    AddToCartAPI,
+    CartViewAPI,
+    DeleteItemCart,
+    UpdateItemCart,
+    Checkout,
+    PlaceOrderView,
+    ConfirmOrderView,
+    ApplyCouponView,
+)
 
 
 urlpatterns = [
     path("add-to-cart/", AddToCartAPI.as_view(), name="add-to-cart"),
     path("cart/", CartViewAPI.as_view(), name="cart"),
-    path("delete-product/",DeleteItemCart.as_view(),name="delete-product"),
-    path("update-product/",UpdateItemCart.as_view(),name="update-product"),
-    path("checkout/<str:slug>/",Checkout.as_view(),name="checkout"),
-    path("checkout/", EditAddress.as_view(), name="editaddress"),
-    path("order-end/",PlaceOrderView.as_view(),name="order"),
-    path("orderconfirm/<int:id>",ConfirmOrderView.as_view(),name="order-confirm"),
-    # path("checkout/", CreateAddress.as_view(), name="createaddress"),
+    path("delete-product/", DeleteItemCart.as_view(), name="delete-product"),
+    path("update-product/", UpdateItemCart.as_view(), name="update-product"),
+    path("checkout/", Checkout.as_view(), name="checkout"),
+    path("order-end/", PlaceOrderView.as_view(), name="order"),
+    path("orderconfirm/<int:id>", ConfirmOrderView.as_view(), name="order-confirm"),
+    path("discount/", ApplyCouponView.as_view(), name="coupon"),
 ]
